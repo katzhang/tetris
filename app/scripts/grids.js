6c0f63c0c4282c332a211fc07a6aed34a4d2b914
katzhang <arcadychang@gmail.com>
2014-06-09 22:54:54 -0400

    added simple canvas to grids constructor

--- app/scripts/grids.js
+++ app/scripts/grids.js
@@ -1,12 +1,26 @@
 'use strict';
 
-function Grids() {
+//global variables
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+function Grids(options) {
 	this.width = 1;
 	this.height = 1;
+	this.posX = null;
+	this.posY = null;
 
-	for(var n in arguments[0]) {
-		this[n] = arguments[0][n];
+	for(var n in options) {
+		this[n] = options[n];
 	}
+
+	this.init = function() {
+		console.log('new grid init');
+		ctx.fillRect(this.posX, this.posY, this.width, this.height);
+	};
+
+	this.init();
 }
 
-var grid = new Grids({height: 5, width: 6});
+
+var grid = new Grids({height: 20, width: 100, posX: 50, posY: 50});
