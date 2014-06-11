dbcc2734a494658640edcd831b08f3e0f25bec5b
katzhang <arcadychang@gmail.com>
2014-06-10 22:35:39 -0400

    added drawGrid, gridsize and stroke constants

--- app/scripts/grids.js
+++ app/scripts/grids.js
@@ -3,6 +3,17 @@
 //global variables
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
+var gridSize = 20;
+var gridStroke = 2;
+
+function drawGrid(posX, posY) {
+	ctx.fillStyle = 'red';
+	ctx.fillRect(posX, posY, gridSize, gridSize);
+
+	ctx.strokeStyle = '#000000';
+	ctx.lineWidth = gridStroke;
+	ctx.strokeRect(posX, posY, gridSize, gridSize);
+}
 
 function Grids(options) {
 	this.width = 1;
@@ -16,11 +27,20 @@ function Grids(options) {
 
 	this.init = function() {
 		console.log('new grid init');
-		ctx.fillRect(this.posX, this.posY, this.width, this.height);
+		var numberX = this.width;
+		var numberY = this.height;
+		var posX = this.posX;
+		var posY = this.posY;
+
+		for(var i = 0; i < numberX; i++) {
+			for(var j = 0; j < numberY; j++) {
+				drawGrid(posX + i * gridSize, posY + j * gridSize);
+			}
+		}
 	};
 
 	this.init();
 }
 
 
-var grid = new Grids({height: 20, width: 100, posX: 50, posY: 50});
+var grid = new Grids({height: 2, width: 5, posX: 50, posY: 50});
